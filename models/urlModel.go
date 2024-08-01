3d04f98bcfad75b53975b6f4ce17b1729f340d56
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-08-01 16:32:07 -0400

    create structs for urls and factory function

--- models/urlModel.go
+++ models/urlModel.go
@@ -1,12 +1,37 @@
 package models
 
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	"jeorozco.com/go/url-shortener/services"
+)
+
 type ShortURL struct {
-	ID          int64  `json:"id"`
-	OriginalURL string `json:"original_url"`
-	ShortURL    string `json:"short_url"`
-	CreatedAt   string `json:"created_at"`
+	ID          string    `json:"id"`
+	OriginalURL string    `json:"original_url"`
+	NewURL      string    `json:"short_url"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+func New(url LongURL) ShortURL {
+	encoded := services.UUIDToShortID(uuid.New())
+	newURL := fmt.Sprintf("localhost:8080/%s", encoded)
+	shortUrl := ShortURL{
+		ID:          encoded,
+		OriginalURL: url.Url,
+		NewURL:      newURL,
+		CreatedAt:   time.Now().UTC(),
+	}
+	return shortUrl
 }
 
 type LongURL struct {
 	Url string `json:"url"`
 }
+
+var UrlCache = make(map[string]ShortURL)
+
+var CacheMutex sync.RWMutex
