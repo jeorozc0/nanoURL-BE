327afc6119313f213eaf2431ed19b1603715ca61
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-08-01 16:35:19 -0400

    create routes for post and get

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module jeorozco.com/go/url-shortener
 
 go 1.22.5
+
+require github.com/google/uuid v1.6.0 // direct
--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.HandleRoot)
 
+	mux.HandleFunc("POST /url", handlers.CreateURL)
+	mux.HandleFunc("GET /{id}", handlers.GetURL)
 	fmt.Println("Server listening to :8080")
 	http.ListenAndServe(":8080", mux)
 }
