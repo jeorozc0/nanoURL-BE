5f59d9054c1842866d7898f052f7d72692f2f0fd
jeorozc0 <jeorozco@bsu.edu>
2024-08-03 20:46:01 -0400

    create quit handler and subroutines for server

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.22.5
 
 require github.com/google/uuid v1.6.0 // direct
 
-require github.com/rs/cors v1.11.0 // indirect
+require github.com/rs/cors v1.11.0 // direct
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/rs/cors"
 	"jeorozco.com/go/url-shortener/handlers"
@@ -22,6 +27,31 @@ func main() {
 	})
 
 	handler := c.Handler(mux)
-	fmt.Println("Server listening to :8080")
-	http.ListenAndServe(":8080", handler)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+
+	go func() {
+		fmt.Println("Server listening to :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v\n", err)
+		}
+	}()
+	// Wait for interrupt signal to gracefully shutdown the server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Server is shutting down...")
+
+	// Create a deadline to wait for
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Doesn't block if no connections, but will otherwise wait until the timeout deadline
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Error shutting down server: %v\n", err)
+	}
+
+	log.Println("Server exited properly")
 }
